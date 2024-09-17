4aa7fadfce1f655b08a17df4763fb6270291116a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-09-17 07:54:55 -0300

    Get argument line first and extract bot identify to define log file name

--- __main__.py
+++ __main__.py
@@ -3,16 +3,17 @@ import logging
 from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes
 
+# Get the token from environment variable
+telegram_token = sys.argv[1]
+telegran_identify = telegram_token.split(':')[0]
+
 # Set up logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    filename="logfile.log"
+    filename=f"{telegran_identify}.log"
 )
 logger = logging.getLogger(__name__)
-
-# Get the token from environment variable
-telegram_token = sys.argv[1]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.info(f"{update.message.date}: {update.message.from_user.id} - {update.message.text}")
